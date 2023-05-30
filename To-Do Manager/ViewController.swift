361668f88bf42826434b971643a232f3df371fc4
AlexseyLoginov <devileean@gmail.com>
2023-05-30 21:48:03 +0300

    initial

---
+++ To-Do Manager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-Do Manager
+//
+//  Created by Алексей Логинов on 30.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
